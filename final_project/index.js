30f20cf31bdbc45d8bced8f4d15be75e1634b232
Piyusha Bauskar <piyushabauskar19@gmail.com>
2025-03-04 18:25:02 +0530

    Book review final commit

--- final_project/index.js
+++ final_project/index.js
@@ -11,12 +11,29 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    const token  = req.headers.authorization.split(" ")[1]
+    console.log(token)
+    if(token) { //get the authorization object stored in the session
+        // token = req.session.authorization['accessToken']; //retrieve the token from authorization object
+        jwt.verify(token, "access",(err,user)=>{ //Use JWT to verify token
+            if(!err){
+                console.log(user)
+                req.user = user;
+                next();
+            }
+            else{
+                return res.status(403).json({message: "User not authenticated"})
+            }
+         });
+     } else {
+         return res.status(403).json({message: "User not logged in"})
+     }
 });
- 
-const PORT =5000;
+
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT,()=>{console.log("Server is running on port " + PORT)});
